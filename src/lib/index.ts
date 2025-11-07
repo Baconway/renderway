abaf6ea83c9512851cb52f82e6aeef6f821e25e2
Baconway <155370111+Baconway@users.noreply.github.com>
2025-11-07 11:54:26 +0700

    fixed type issues

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
 // place files you want to import through the `$lib` alias in this folder.
 import * as osu from 'osu-api-v2-js';
-import { OSU_CLIENT_SECRET } from '$env/static/private';
+import { ComSpec, OSU_CLIENT_SECRET } from '$env/static/private';
 
 const api = await osu.API.createAsync(45354, OSU_CLIENT_SECRET);
 api.refresh_token_on_expires = true;
 
-export function testcall(username: string) {
-	console.log('did i do this right?');
-	//console.log(api.getUser);
+export async function testcall(username: string) {
+	const user = await api.getUser(username, osu.Ruleset.mania);
+	return user.country;
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,12 @@
 import type { Actions } from './$types';
 import { testcall } from '$lib';
+import { stringify } from 'querystring';
 
 export const actions = {
 	submit: async ({ request }) => {
-		const data = await request.formData();
-		console.log(data.get('name'));
+		const data: FormData = await request.formData();
+		console.log(data);
+		const username = data.get('name') as string;
+		console.log(await testcall(username));
 	}
 };
