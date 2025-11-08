cab3170ddec64f6be88199dbffe47d49f8ab8d8a
Baconway <155370111+Baconway@users.noreply.github.com>
2025-11-08 16:59:47 +0700

    additional layout changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
-<div class="flex flex-col items-center justify-center">
+<div class="flex flex-col items-center justify-center gap-5 text-white">
 	<h1>osu! profile render thingamabob</h1>
 	<form class="flex flex-col" method="POST" autocomplete="off" action="?/submit">
-		<input type="text" name="name" />
-		<button> test button</button>
+		<input
+			class="border-2 border-t-0 border-r-0 border-l-0 border-b-white bg-transparent text-black focus:outline-amber-500"
+			type="text"
+			name="name"
+		/>
+		<button>test button</button>
 	</form>
+
+	<div
+		class="flex flex-col gap-5
+	md:flex-row
+	"
+	>
+		<img alt="template" />
+		<img alt="template" />
+		<img alt="template" />
+	</div>
 </div>
