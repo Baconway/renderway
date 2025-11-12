adda8932f72961ef8e238e096464cc6cfc196433
Baconway <155370111+Baconway@users.noreply.github.com>
2025-11-12 13:44:06 +0700

    svg styles

--- src/routes/render/+page.svelte
+++ src/routes/render/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	let { data }: PageProps = $props();
-	console.log(data);
+	console.log(data.profile_colour);
+	const profileColor = `hsl(${data.profile_hue}, 100%, 0%)`;
 </script>
 
 {#snippet card()}
-	<svg width="600" height="300">
-		<rect x="0" y="0" width="250" height="250"></rect>
-		<text fill="white" x="15" y="15">dqbyhuwdqhyuidhqyu</text>
+	<svg width="600" height="400">
+		<rect width="600" height="400"></rect>
+
+		<image x="0" y="0" width="600" href={data.cover?.custom_url}></image>
+
+		<g
+			><rect width="600" height="125" y="95"></rect>
+			<image
+				x="10"
+				y="95"
+				width="125"
+				height="125"
+				href={data.avatar_url}
+				clip-path="inset(0% round 6px)"
+			></image>
+			<text fill="white" x="200" y="125">{data.username}</text>
+		</g>
 	</svg>
 {/snippet}
 
