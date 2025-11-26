4e2d8062c532b2a295109c9d3d86f2956a212100
Baconway <Baconway@users.noreply.github.com>
2025-11-26 22:37:33 +0700

    redid response headers

--- src/routes/render/+server.ts
+++ src/routes/render/+server.ts
@@ -1,5 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = () => {
-	return new Response('/discord.svg');
+	return new Response('/discord.svg', {
+		status: 200,
+		statusText: 'idk what to put here',
+		headers: new Headers({
+			'content-type': 'image/svg+xml'
+		})
+	});
 };
