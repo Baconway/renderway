05b41c4226df892b605b6e80f93211fb3a974501
Baconway <Baconway@users.noreply.github.com>
2025-11-27 11:05:13 +0700

    test

--- src/routes/render/+server.ts
+++ src/routes/render/+server.ts
@@ -1,6 +1,7 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
+import { type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = () => {
+export const GET: RequestHandler = ({ url }) => {
+	console.log(url.pathname, url.searchParams);
 	return new Response(JSON.stringify('/discord.svg'), {
 		status: 200,
 		statusText: 'idk what to put here',
